d9db4b5b822e78521b182b01f3a41091bd897639
DBeloni <davi.beloni.pinheiro@gmail.com>
2026-07-13 15:59:59 -0300

    fix(footer): força sincronização do componente footer com a branch develop

--- script/components/site-footer.js
+++ script/components/site-footer.js
@@ -1,21 +1,27 @@
-const estaLogado = localStorage.getItem("usuarioLogado") === "true";
+class SiteFooter extends HTMLElement {
+    connectedCallback() {
+        const estaLogado = localStorage.getItem("usuarioLogado") === "true";
 
-this.innerHTML = `
-<link rel="stylesheet" href="/style/css/components/footer.css">
-<footer class="rodapeGeral py-4">
-    <div class="container text-center">
-        <ul class="nav justify-content-center mb-3 lista-links-footer">
-            <li><a href="/index.html" class="nav-link px-3">${getTexto('nav_home')}</a></li>
-            <li><a href="/pages/about.html" class="nav-link px-3">${getTexto('nav_about')}</a></li>
-            
-            ${estaLogado ? `<li><a href="/pages/map.html" class="nav-link px-3">${getTexto('nav_map')}</a></li>` : ''}
-            
-            ${!estaLogado ? `
-                <li><a href="/pages/register.html" class="nav-link px-3">${getTexto('nav_register')}</a></li>
-                <li><a href="/pages/login.html" class="nav-link px-3">${getTexto('nav_login')}</a></li>
-            ` : ''}
-        </ul>
-        <p class="mb-0">&copy; 2026 PlaceFinder, Inc</p>
-    </div>
-</footer>
-`;
+        this.innerHTML = `
+        <link rel="stylesheet" href="/style/css/components/footer.css">
+        <footer class="rodapeGeral py-4">
+            <div class="container text-center">
+                <ul class="nav justify-content-center mb-3 lista-links-footer">
+                    <li><a href="/index.html" class="nav-link px-3">Home</a></li>
+                    <li><a href="/pages/about.html" class="nav-link px-3">About</a></li>
+                    
+                    ${estaLogado ? `<li><a href="/pages/map.html" class="nav-link px-3">Map</a></li>` : ''}
+                    
+                    ${!estaLogado ? `
+                        <li><a href="/pages/register.html" class="nav-link px-3">Register</a></li>
+                        <li><a href="/pages/login.html" class="nav-link px-3">Login</a></li>
+                    ` : ''}
+                </ul>
+                <p class="mb-0">© 2026 PlaceFinder, Inc</p>
+            </div>
+        </footer>
+        `;
+    }
+}
+
+customElements.define("site-footer", SiteFooter);
